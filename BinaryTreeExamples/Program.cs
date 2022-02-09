76856cd8e1deee459a85e10a236b302dcb5fe906
User <User@TEACHER3>
2022-02-09 11:47:09 +0200

    Add project files.

---
+++ BinaryTreeExamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DataStructureCore;
+namespace BinaryTreeExamples
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+          
+          
+
+            BinNode<int> t = new BinNode<int>(54);
+            BinNode<int> left = new BinNode<int>(null, 48, new BinNode<int>(54));
+            t.SetLeft(left);
+            BinNode<int> right = new BinNode<int>(new BinNode<int>(63), 77, null);
+            t.SetRight(right);
+
+            Console.WriteLine(t);
+           
+          
+       
+            
+           
+        }
+    }
+}
