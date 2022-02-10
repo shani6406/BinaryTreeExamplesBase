6fca669275a3860dc2bbaaaa0902e5a898c78ec1
stma1one <42409551+stma1one@users.noreply.github.com>
2022-02-10 23:18:03 +0200

    Update BTHelper.cs

--- BinaryTreeExamples/BTHelper.cs
+++ BinaryTreeExamples/BTHelper.cs
@@ -8,6 +8,8 @@ namespace BinaryTreeExamples
     //כאן יופיעו כל הפעולות העזר עבור עצים
     class BTHelper
     {
+        #region Test
+            #end region
         #region יצירת עץ
         public static BinNode<int> CreateTree()
         {
